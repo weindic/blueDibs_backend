f6e2a470fa4f31fbe5ce37ba9beb7d3efabbb6cc
Weindic <weindic.inc@gmail.com>
2024-06-26 22:11:39 +0530

    mohit

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,39 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as admin from 'firebase-admin';
+import {  AppModuleexport } from './app.module'; // Correct import of AppModule
+
+import * as admin from 'firebase-admin'; // Import Firebase Admin if needed
 import { json, urlencoded } from 'express';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModuleexport);
 
-    app.use(json({ limit: '50mb' }));
-    app.use(urlencoded({ limit: '50mb' }));
-    const options:CorsOptions = {
-        origin: ['http://localhost:8100'], // Specify allowed origin(s)
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Specify allowed HTTP methods
-        allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-        exposedHeaders: ['Authorization'], // Specify exposed headers (if needed)
-        credentials: true, // Allow credentials (e.g., cookies and authorization headers)
-        optionsSuccessStatus: 200, // Success status for preflight OPTIONS requests
-    }
-    // Enable CORS with specific options
-    app.enableCors(options);
+  // Firebase Admin initialization if needed
+  // admin.initializeApp();
 
-    console.log(process.env.PORT)
-    await app.listen(parseInt(process.env.PORT as string));
+  // Middleware for handling JSON and URL encoded data
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb' }));
+
+  // CORS configuration
+  const options: CorsOptions = {
+    origin: ['http://localhost:8100'], // Specify allowed origin(s)
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    exposedHeaders: ['Authorization'], // Specify exposed headers (if needed)
+    credentials: true, // Allow credentials (e.g., cookies and authorization headers)
+    optionsSuccessStatus: 200, // Success status for preflight OPTIONS requests
+  };
+
+  // Enable CORS with specific options
+  app.enableCors(options);
+
+  // Read and use environment variable for port, fallback to 3000 if not set
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
